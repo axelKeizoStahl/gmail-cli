b0dde3b3c7ffaa109a331c6304657b6d9365a827
axelKeizoStahl <axelkeizo@gmail.com>
2023-06-23 14:41:41 -0400

    move into token folder

--- src/ui/credentials.py
+++ src/ui/credentials.py
@@ -6,11 +6,11 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-def getcreds(SCOPES, path):
+def getcreds(path, SCOPES):
     creds = None
 
-    if os.path.exists(f'../{path}/token.json'):
-        creds = Credentials.from_authorized_user_file(f'../{path}/token.json', SCOPES)
+    if os.path.exists('tokens/token.json'):
+        creds = Credentials.from_authorized_user_file('tokens/token.json', SCOPES)
 
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -19,6 +19,6 @@ def getcreds(SCOPES, path):
             flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
 
-        with open(f'../{path}/token.json', 'w') as token:
+        with open('tokens/token.json', 'w') as token:
             token.write(creds.to_json())
     return creds
