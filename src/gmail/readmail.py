a5cecafa12510ef2f626d8ecda4c8cc87c6ac808
axelKeizoStahl <axelkeizo@gmail.com>
2023-06-26 22:44:10 -0400

    read inbox, different folders, dates, sizes, search quieries added

---
+++ src/gmail/readmail.py
@@ -0,0 +1,24 @@
+import os
+from googleapiclient import build
+from datetime import datetime, timedelta
+
+#SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+def read(creds, amount, label='INBOX', date=None):
+    service = build('gmail', 'v1', credentials=creds)
+    if not date: date = datetime.now().date()
+    date = datetime.strptime(date, '%Y-%m-%d').date()
+    date = date.strftime('%y-%m-%dT00:00:00Z')
+    query = f'before:{date}'
+    results = service.users().messages.list(userId='me', q=query, labelIds=[label], maxResults=amount).execute()
+    messages = results.get('messages', [])
+    return [ service.users().messages().get(userId='me', id=message['id']).execute() for message in messages]
+
+
+
+def search(creds, string, amount=10):
+    service = build('gmail', 'v1', credentials=creds)
+    string = f'"{string}"'
+    results = service.users().messages.list(userId='me', q=string, maxResults = amount).execute()
+    messages = results.get('messages', [])
+    return [service.users().messages().get(userId='me', id=message['id']).execute() for message in messages]
